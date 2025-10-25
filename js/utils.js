a05e81c6a5b9574d08a47807a7a37c3e37ccd865
WJ Lu <AlfredWJLu@gmail.com>
2025-10-25 09:56:55 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -70,7 +70,7 @@ function loadTitle(div_name = "title-container") {
         "           E-mail: AlfredWJLu AT gmail DOT com<br/>",
         "           Wechat: alfredwjlu<br/>",
         "           <br/>",
-        "            More about me like educations, publications so on so forth (<a href=\"doc/LuwjCVEn.pdf\">en</a>)<br/>",
+        "             <a href=\"doc/LuwjCVEn.pdf\">More about me...</a><br/>",
         "            <!-- <a href=\"paps.php\">Projects and Publications</a> -->",
         "       </td>",
         "       </tr>",
